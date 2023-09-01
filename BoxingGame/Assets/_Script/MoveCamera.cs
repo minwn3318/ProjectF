d8529f6e13e7e68c711080f214bb10e9a92d92a6
geunyong12 <minwn3318@naver.com>
2023-09-01 10:25:42 +0900

    camera set

---
+++ BoxingGame/Assets/_Script/MoveCamera.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 cameraPosition = new Vector3(0, 5, -8);
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + cameraPosition;
+    }
+}
